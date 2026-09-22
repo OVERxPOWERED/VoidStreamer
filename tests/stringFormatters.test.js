37e51d60f49ec43d9a9a595895055206e57a5c9d
overxpowered <6burhanuddin6@gmail.com>
2026-09-23 00:44:55 +0530

    chore: remove ephemeral stringFormatters test script

--- tests/stringFormatters.test.js
+++
@@ -1,7 +0,0 @@
-// Unit tests: stringFormatters
-import { slugify, camelCase } from '../utils/stringFormatters.js';
-
-console.log('Testing slugify:', slugify('Hello World!') === 'hello-world');
-console.log('Testing camelCase:', camelCase('hello-world') === 'helloWorld');
-
-// Revision 8 - 2026-09-21
